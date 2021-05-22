815e10553a09dcb137345f089d46c64cfe381454
Sawitha <sawitha@mamiteam.com>
2021-05-23 00:23:42 +0700

    add scenario create new user

--- src/test/java/features/PostGetPutDeleteMethod.feature
+++ src/test/java/features/PostGetPutDeleteMethod.feature
@@ -1,5 +1,6 @@
-
 Feature: Verify Go Rest HTTPS Methods using REST-assured
+
+  //Get user details
 
   @GET
   Scenario: GET - Verify API response of users data
@@ -7,11 +8,25 @@ Feature: Verify Go Rest HTTPS Methods using REST-assured
     And User authenticates the API request with token
     When User sends the API request to get all the list
     Then User validates the response status is 200
-    And User validates the response meta pagination "total" is 1478
-    And User validates the response meta pagination "pages" is 74
+    And User validates the response meta pagination "total" is 1484
+    And User validates the response meta pagination "pages" is 75
     And User validates the response meta pagination "page" is 1
     And User validates the response meta pagination "limit" is 20
     And User validates the list body response equals with 20
-    And User validates the body response with name "Fr. Dayaanidhi Tandon"
+#    And User validates the body response with name "Fr. Dayaanidhi Tandon"
 
+  @POST
+  Scenario Outline: POST - Create new user
+    Given User sets the base API request "/users"
+    When User create new data user with POST request "/users" and request body "<name>", "<gender>", "<email>", "<status>"
+    Then User validates the response status is 200
+    And User validates id is not null
+    And User validates the body response with name "<name>"
+    And User validates the body response with gender "<gender>"
+    And User validates the body response with email "<email>"
+    And User validates the body response with status "<status>"
+
+    Examples:
+      | name          | gender | email                  | status |
+      | Justin Bieber | Male   | justin.bieber@fake.com | Active |
 
--- src/test/java/steps/PostGetPutDeleteSteps.java
+++ src/test/java/steps/PostGetPutDeleteSteps.java
@@ -1,12 +1,15 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
 import org.testng.Assert;
 
 import java.util.List;
@@ -15,7 +18,8 @@ import static io.restassured.RestAssured.given;
 
 public class PostGetPutDeleteSteps {
 
-    private static final String TOKEN = "15468ae613c31ba7242ea5f19c85d6bc18d60874897e0e872501e866c0b271e6";
+
+    private static final String TOKEN = "26c2934debb54e5000fa6469d822e36565fc83cbf4feb2805444ef18644c50f1";
     private static final String BASE_URI = "https://gorest.co.in/public-api";
 
     RequestSpecification request;
@@ -43,12 +47,12 @@ public class PostGetPutDeleteSteps {
 
     @And("User validates the body response with name {string}")
     public void userValidatesTheBodyResponseWithName(String name) {
-        Assert.assertEquals(response.body().jsonPath().get("data.name[2]"), name, "Something was wrong! Response name not equals with '"+ name +"'! ");
+        Assert.assertEquals(response.body().jsonPath().get("data.name[0]"), name, "Something was wrong! Response name not equals with '" + name + "'! ");
     }
 
     @And("User validates the response meta pagination {string} is {int}")
     public void userValidatesTheResponseMetaPaginationIs(String meta, int value) {
-        Assert.assertEquals(response.jsonPath().getInt("meta.pagination.'"+ meta +"'"), value, "Something was wrong! Please check the value number");
+        Assert.assertEquals(response.jsonPath().getInt("meta.pagination.'" + meta + "'"), value, "Something was wrong! Please check the value number");
     }
 
     @And("User validates the list body response equals with {int}")
@@ -57,5 +61,41 @@ public class PostGetPutDeleteSteps {
         int list = jsonResponse.size();
 
         Assert.assertEquals(list, listData, "Something was wrong! Please check the list response");
+    }
+
+    @When("User create new data user with POST request {string} and request body {string}, {string}, {string}, {string}")
+    public void userCreateNewDataUserWithPOSTRequestAndRequestBodyNameGenderEmailStatus(String url, String name, String gender, String email, String status) {
+
+        JSONObject requestParams = new JSONObject();
+
+        requestParams.put("name", name);
+        requestParams.put("gender", gender);
+        requestParams.put("email", email);
+        requestParams.put("status", status);
+
+        request = given().header("Authorization", "Bearer " + TOKEN).contentType(ContentType.JSON).with().body(requestParams);
+        request.body(requestParams.toJSONString());
+        response = request.post(BASE_URI + url);
+        response = request.get().then().log().all().extract().response();
+    }
+
+    @And("User validates id is not null")
+    public void userValidatesIdIsNotNull() {
+        Assert.assertTrue(response.jsonPath().get("data.id[0]") != null, "data.id" + " is empty");
+    }
+
+    @And("User validates the body response with gender {string}")
+    public void userValidatesTheBodyResponseWithGender(String gender) {
+        Assert.assertEquals(response.body().jsonPath().get("data.gender[0]"), gender, "Something was wrong! Response name not equals with '" + gender + "'! ");
+    }
+
+    @And("User validates the body response with email {string}")
+    public void userValidatesTheBodyResponseWithEmail(String email) {
+        Assert.assertEquals(response.body().jsonPath().get("data.email[0]"), email, "Something was wrong! Response name not equals with '" + email + "'! ");
+    }
+
+    @And("User validates the body response with status {string}")
+    public void userValidatesTheBodyResponseWithStatus(String status) {
+        Assert.assertEquals(response.body().jsonPath().get("data.status[0]"), status, "Something was wrong! Response name not equals with '" + status + "'! ");
     }
 }
